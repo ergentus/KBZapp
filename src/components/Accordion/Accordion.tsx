88d645ba8a445c932fbf2e3f5f78dd98acd93927
ergentus <ponfable999@gmail.com>
2023-02-25 19:56:58 +0300

    first commit

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+type AccordionPropsType = {
+   titleValue: string,
+   collapsed: boolean
+}
+
+export function Accordion(props: AccordionPropsType) {
+   console.log('Accordion rendered');
+
+   if (props.collapsed === true) {
+      return (
+         <div>
+            <AccordionTitle title={props.titleValue} />
+            <AccordionBody />
+         </div>
+      )
+   } else {
+      return (
+         <div>
+            <AccordionTitle title={props.titleValue} />
+         </div>
+      )
+   }
+}
+
+
+type AccordionTitlePropsType = {
+   title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+   console.log('AccordionTitle rendering');
+   return <h3>{props.title}</h3>
+}
+
+function AccordionBody(props: any) {
+   console.log('AccordionBody rendering');
+   return (
+      <ul>
+         <li>1</li>
+         <li>2</li>
+         <li>3</li>
+      </ul>
+   )
+}
+
+export default Accordion;
